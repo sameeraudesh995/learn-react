8727ddbaa1280aa363663cc9935502c6b2377988
sameeraudesh995 <sameeraudesh.2225@gmail.com>
2025-02-18 20:06:45 +0530

    init

---
+++ src/ui/SayHi.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SayHi =()=>{
+    return(
+        <div>
+            <h1>
+                say hi
+            </h1>
+        </div>
+    )
+}
+export default SayHi
